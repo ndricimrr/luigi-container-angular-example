0044d8ef3e11bb52b4d10199f58e641eb4976805
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-11-08 15:08:58 +0100

    Add example with verbose console

---
+++ src/app/app.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { LuigiContainer, LuigiCompoundContainer } from '@luigi-project/container';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-lui-component';
+  public compoundConfig = {
+    renderer: {
+      use: 'grid',
+      config: {
+        columns: '1fr 1fr 1fr 2fr',
+        layouts: [
+          {
+            minWidth: 0,
+            maxWidth: 600,
+            columns: '1fr',
+            gap: 0
+          },
+          {
+            minWidth: 600,
+            maxWidth: 1024,
+            columns: '1fr 1fr',
+            gap: '30px'
+          }
+        ]
+      }
+    },
+    children: [
+      {
+        viewUrl: 'https://luigiwebcomponents.gitlab.io/layouts/panelHeader.js',
+        context: {
+          title: 'My Awesome Grid',
+          description: 'Really awesome'
+        },
+        layoutConfig: {
+          row: '1',
+          column: '1 / -1'
+        },
+        eventListeners: [
+          {
+            source: 'input1',
+            name: 'sendInput',
+            action: 'update',
+            dataConverter: (data: string) => {
+              return 'new text: ' + data;
+            }
+          }
+        ]
+      },
+      {
+        id: 'input1',
+        viewUrl: 'https://luigiwebcomponents.gitlab.io/util/input.js',
+        context: {
+          title: 'Some input',
+          instant: true
+        }
+      },
+    ]
+  };
+
+  constructor() {
+    customElements.define('luigi-container', LuigiContainer);
+    customElements.define('luigi-compound-container-my-name', LuigiCompoundContainer);
+  }
+
+  ngAfterViewInit() {
+
+    (document.querySelector('luigi-compound-container-my-name') as any).compoundConfig = this.compoundConfig;
+    // this should not be needed but it seems to be so ! Uncommenting the line below the compound container never initializes !
+    (document.querySelector('luigi-compound-container-my-name') as any).init();
+  }
+}
